edd3b1fad379711e4042091ab4652c093b75ca2c
valentingonzalez <gonzalezvalencho@gmail.com>
2018-09-15 14:01:08 -0300

    contact page

--- wp-content/themes/blance-child/functions.php
+++ wp-content/themes/blance-child/functions.php
@@ -6,3 +6,17 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
 // END ENQUEUE PARENT ACTION
+
+add_action( 'wp_enqueue_scripts', 'generate_remove_scripts' );
+function generate_remove_scripts() 
+{
+    wp_dequeue_style( 'generate-child' );
+}
+
+add_action( 'wp_enqueue_scripts', 'generate_move_scripts', 999 );
+function generate_move_scripts() 
+{
+    if ( is_child_theme() ) :
+        wp_enqueue_style( 'generate-child', get_stylesheet_uri(), true, filemtime( get_stylesheet_directory() . '/style.css' ), 'all' );
+    endif;
+}
